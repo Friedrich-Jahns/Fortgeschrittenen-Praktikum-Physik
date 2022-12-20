9ed42ce8cce75be007f5887e157acb4eb655e48a
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2022-12-20 13:44:19 +0100

    save

--- FP7_Rutherford/FP7_lib/functions.py
+++ FP7_Rutherford/FP7_lib/functions.py
@@ -1,15 +1,20 @@
-def pos(x):
-    for i in range(len(data[0])):
-        if data[0,i]>=x:
+import numpy as np
+import lmfit
+import matplotlib.pyplot as plt
+
+def pos(x,dat):
+    for i in range(len(dat[0])):
+        if np.array(dat)[0,i]>=x:
             return i
             break
 
 
 
 
-def gaus1fit(von,bis,path,m=1,mmin=0,mmax=2,s=0.2,smax=1000,scale=1e-9,override=False): 
-    vonarr = pos(von)
-    bisarr = pos(bis)
+def gaus1fit(dat,von,bis,path,m=1,mmin=0,mmax=2,s=0.2,smax=1000,scale=1e-9,override=False): 
+    data=np.array(dat[1:4])
+    vonarr = pos(von,data)
+    bisarr = pos(bis,data)
 
     model_gaus = lmfit.models.GaussianModel()  
     pars_gaus=model_gaus.guess(data=data[1,vonarr:bisarr],x=data[0,vonarr:bisarr])
@@ -23,12 +28,9 @@ def gaus1fit(von,bis,path,m=1,mmin=0,mmax=2,s=0.2,smax=1000,scale=1e-9,override=
     out_gaus = model_gaus.fit(data[1,vonarr:bisarr],x=data[0,vonarr:bisarr],weights=1/data[2,vonarr:bisarr],params=pars_gaus,nan_policy='propagate')
 
     fig = plt.figure(figsize=(8,6),dpi=80,linewidth=50)
-    plt.errorbar(data[0,vonarr:bisarr],yscale*data[1,vonarr:bisarr],yerr=yscale*data[2,vonarr:bisarr],fmt='o',ecolor='gray',color='r', elinewidth=3, capsize=0,zorder=5,label="Daten")
-    plt.plot(np.arange(von,bis,0.01),yscale*out_gaus.eval(x=np.arange(von,bis,0.01)),zorder=10,color='k',linestyle='-',alpha=0.5,label="Gaus-Fit")
+    plt.errorbar(data[0,vonarr:bisarr],data[1,vonarr:bisarr],yerr=data[2,vonarr:bisarr],fmt='o',ecolor='gray',color='r', elinewidth=3, capsize=0,zorder=5,label="Daten")
+    plt.plot(np.arange(von,bis,0.01),out_gaus.eval(x=np.arange(von,bis,0.01)),zorder=10,color='k',linestyle='-',alpha=0.5,label="Gaus-Fit")
     
-
-
-
 
     print(out_gaus.fit_report())
 
@@ -39,8 +41,8 @@ def gaus1fit(von,bis,path,m=1,mmin=0,mmax=2,s=0.2,smax=1000,scale=1e-9,override=
 
 
 def gaus3fit(von,bis,path):
-    vonarr = pos(von)
-    bisarr = pos(bis)
+    vonarr = pos(von,dat)
+    bisarr = pos(bis,dat)
 
 
 
