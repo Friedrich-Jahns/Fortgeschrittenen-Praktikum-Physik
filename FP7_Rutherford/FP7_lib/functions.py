86ccfafa0e1533c85ca1bd2f0c4abde420b0d72b
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2022-12-20 14:40:32 +0100

    save

--- FP7_Rutherford/FP7_lib/functions.py
+++ FP7_Rutherford/FP7_lib/functions.py
@@ -16,24 +16,25 @@ def gaus1fit(dat,von,bis,path,m=1,mmin=0,mmax=2,s=0.2,smax=1000,scale=1e-9,overr
     vonarr = pos(von,data)
     bisarr = pos(bis,data)
 
-    model_gaus = lmfit.models.GaussianModel()  
+    model_gaus = lmfit.models.GaussianModel()
     pars_gaus=model_gaus.guess(data=data[1,vonarr:bisarr],x=data[0,vonarr:bisarr])
+
     if override:
         pars_gaus["center"].set(value=m,min=mmin,max=mmax)
         pars_gaus["sigma"].set(value=1,min=0)
         pars_gaus["amplitude"].set(value=1,min=0)
 
-
-    #Fitten
     out_gaus = model_gaus.fit(data[1,vonarr:bisarr],x=data[0,vonarr:bisarr],weights=1/data[2,vonarr:bisarr],params=pars_gaus,nan_policy='propagate')
 
     fig = plt.figure(figsize=(8,6),dpi=80,linewidth=50)
-    plt.errorbar(data[0,vonarr:bisarr],data[1,vonarr:bisarr],yerr=data[2,vonarr:bisarr],fmt='o',ecolor='gray',color='r', elinewidth=3, capsize=0,zorder=5,label="Daten")
-    plt.plot(np.arange(von,bis,0.01),out_gaus.eval(x=np.arange(von,bis,0.01)),zorder=10,color='k',linestyle='-',alpha=0.5,label="Gaus-Fit")
+    plt.errorbar(data[0,vonarr:bisarr],data[1,vonarr:bisarr],yerr=data[2,vonarr:bisarr],fmt='.',ecolor='gray',color='r', elinewidth=1.5, capsize=0,zorder=5,label="Daten")
+    plt.plot(np.arange(von,bis,0.01),out_gaus.eval(x=np.arange(von,bis,0.01)),zorder=10,color='k',linestyle='-',alpha=1,label="Gaus-Fit")
     
-
+    plt.grid()
+    plt.legend()
     print(out_gaus.fit_report())
-
+    plt.xlabel('Channel')
+    plt.ylabel('Counts')
 
     return np.array([np.arange(von,bis,0.01),out_gaus.eval(x=np.arange(von,bis,0.01))]), out_gaus
 
@@ -41,8 +42,10 @@ def gaus1fit(dat,von,bis,path,m=1,mmin=0,mmax=2,s=0.2,smax=1000,scale=1e-9,overr
 
 
 def gaus3fit(von,bis,path):
-    vonarr = pos(von,dat)
-    bisarr = pos(bis,dat)
+    data=np.array(dat[1:4])
+    vonarr = pos(von,data)
+    bisarr = pos(bis,data)
 
-
+    model_gaus3 = lmfit.models.GaussianModel(prefix='g1_')+lmfit.models.GaussianModel(prefix='g2_')+lmfit.models.GaussianModel(prefix='g3_')
+    pars_gaus=model_gaus3.guess(data=data[1,vonarr:bisarr],x=data[0,vonarr:bisarr])
 
