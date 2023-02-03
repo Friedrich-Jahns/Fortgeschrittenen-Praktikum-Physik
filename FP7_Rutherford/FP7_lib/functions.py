2726ec42b7811cb9f011f850068b0562596f4fe7
Friedrich-Jahns-PC <jahnsfriedrich@gmail.com>
2023-02-03 13:13:33 +0100

    save

--- FP7_Rutherford/FP7_lib/functions.py
+++ FP7_Rutherford/FP7_lib/functions.py
@@ -138,3 +138,14 @@ def gausfit_table(fit):
                 r"red\chi^2    &" + str(fit.out.redchi) + "\n"
                 r"\end{tabular}" + "\n"
                 r"\end{table}" + "\n\n")
+    
+
+
+
+
+
+def linear_fit(data,x,err):
+    model_lin = lmfit.models.LinearModel()
+    pars_lin = model_lin.guess(data=data,x=x)
+    out_lin =  model_lin.fit(data,x=x,params=pars_lin,nan_policy='propagate',weights=1/np.array(err))
+    return out_lin
